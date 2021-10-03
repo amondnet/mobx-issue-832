f3b8625e145bcb5a05d5b292346b3abd2d87643c
Saurabh Dalakoti <saurabhdalakoti07@gmail.com>
2021-10-03 15:32:21 +0530

    project started and added navigator as well

---
+++ meal_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/categories_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1)
+      ),
+      home: CategoriesScreen(),
+    );
+  }
+}
+
