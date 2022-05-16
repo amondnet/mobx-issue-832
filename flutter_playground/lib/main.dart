7c942a7d5c54360576337340304860773da2fa8a
dalakoti07 <saurabhdalakoti07@gmail.com>
2022-05-16 15:40:11 +0530

    list view inside scroll view

--- flutter_playground/lib/main.dart
+++ flutter_playground/lib/main.dart
@@ -3,46 +3,54 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(home: DemoApp()));
 
 class DemoApp extends StatelessWidget {
-  Widget build(BuildContext context) => Scaffold(body: Signature());
+  Widget build(BuildContext context) => Scaffold(body: SimpleWidget());
 }
 
-class Signature extends StatefulWidget {
-  SignatureState createState() => SignatureState();
-}
+class SimpleWidget extends StatelessWidget{
 
-class SignatureState extends State<Signature> {
-  List<Offset?> _points = <Offset>[];
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onPanUpdate: (DragUpdateDetails details) {
-        setState(() {
-          RenderBox? referenceBox = context.findRenderObject() as RenderBox;
-          Offset localPosition = referenceBox.globalToLocal(details.globalPosition);
-          _points = List.from(_points)..add(localPosition);
-        });
-      },
-      onPanEnd: (DragEndDetails details) => _points.add(null),
-      child: CustomPaint(
-        painter: SignaturePainter(_points),
-        size: Size.infinite,
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 100,),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.star, color: Colors.green[500]),
+                Icon(Icons.star, color: Colors.green[500]),
+                Icon(Icons.star, color: Colors.green[500]),
+                const Icon(Icons.star, color: Colors.black),
+                const Icon(Icons.star, color: Colors.black),
+              ],
+            ),
+            SizedBox(height: 100,),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.star, color: Colors.green[500]),
+                Icon(Icons.star, color: Colors.green[500]),
+                Icon(Icons.star, color: Colors.green[500]),
+                const Icon(Icons.star, color: Colors.black),
+                const Icon(Icons.star, color: Colors.black),
+              ],
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: 20,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('item $index'),
+                );
+              },
+            )
+          ],
+        ),
       ),
     );
   }
+
 }
 
-class SignaturePainter extends CustomPainter {
-  SignaturePainter(this.points);
-  final List<Offset?> points;
-  void paint(Canvas canvas, Size size) {
-    var paint = Paint()
-      ..color = Colors.black
-      ..strokeCap = StrokeCap.round
-      ..strokeWidth = 5.0;
-    for (int i = 0; i < points.length - 1; i++) {
-      if (points[i] != null && points[i + 1] != null)
-        canvas.drawLine(points[i]!, points[i + 1]!, paint);
-    }
-  }
-
-  bool shouldRepaint(SignaturePainter other) => other.points != points;
-}
